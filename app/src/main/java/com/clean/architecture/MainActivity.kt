2ebe02513206c325c3f0b31739cfbc320db0ca85
youeuneyol <ey.yoo@fanmaum.com>
2023-04-29 00:08:36 +0900

    MainActivity NavGraph 구성

--- app/src/main/java/com/clean/architecture/MainActivity.kt
+++ app/src/main/java/com/clean/architecture/MainActivity.kt
@@ -3,29 +3,26 @@ package com.clean.architecture
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.clean.architecture.presentation.navigation.navGraph
 import com.clean.architecture.ui.theme.MovieHolicTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private lateinit var navController: NavHostController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MovieHolicTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                navController = rememberNavController()
+                navGraph(navController = navController)
             }
         }
     }
